460ba549457ac048e71df65b55e9365d1871c4e8
Fredrik Wollsén <fredrik@neam.se>
2014-07-21 15:21:51 +0300

    Moved around files to appropriate locations within related composer deps

---
+++ helpers/WorkflowUiControllerConstants.php
@@ -0,0 +1,23 @@
+<?php
+
+class WorkflowUiControllerConstants
+{
+    // Item actions
+    const ACTION_BROWSE = 'browse';
+    const ACTION_VIEW = 'view';
+    const ACTION_ADD = 'add';
+    const ACTION_EDIT = 'edit';
+    const ACTION_CLONE = 'clone';
+    const ACTION_REMOVE = 'remove';
+    const ACTION_PREVIEW = 'preview';
+    const ACTION_TRANSLATE = 'translate';
+    const ACTION_TRANSLATION_OVERVIEW = 'translationOverview';
+    const ACTION_EVALUATE = 'evaluate';
+    const ACTION_PROOFREAD = 'proofread';
+    const ACTION_APPROVE = 'approve';
+    const ACTION_PUBLISH = 'publish';
+    const ACTION_PREPARE_FOR_REVIEW = 'prepareForReview';
+    const ACTION_PREPARE_FOR_PUBLISHING = 'prepareForPublishing';
+    const ACTION_CANCEL = 'cancel';
+
+} 
